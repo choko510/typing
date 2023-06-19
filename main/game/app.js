47d4ab7d2c2c129ee15bffb104f4a4c19d39e52a
choko510 <dg233033@daisho.ac.jp>
2023-06-19 09:52:41 +0900

    a

--- main/game/app.js
+++ main/game/app.js
@@ -1,80 +1,59 @@
+const word = [
+  "メロンパンってメロン入ってないらしいで",
+  "meronpannttemeronhaittenairasiide",
+  "隣の客は良く柿食う客だ",
+  "tonarinokyakuhayokukakikuukyakuda",
+  "チョコチップメロンパンは高い方が美味しいらしい",
+  "tyokottippumeronnpanhatakaihougaoisiirasii",
+  "コンセントにさす",
+  "konsentonisasu",
+  "蚊取り閃光",
+  "katorisenkou"
+]
 
-  const quotes = [
+let nanmozime; // 現在の引用文のインデックス
+let tangomozisu; // 現在の単語のインデックス
+let nowword; // 現在の単語
+let alltype; // 入力された文字列
+let miss; // 　ミスタイプ数
 
-"retasu",
-"komatuna",
-"hourensou",
-"rennkonn",
-"tamanegi",
-// 他の引用文を追加
-];
+var nownum = 0
+function gamestart(){
+  var nownum = randomnumber(nownum);
+  nowword = word[nownum];
+  nanmozime = 0
+  document.getElementById('text').textContent = word[nownum-1];
+  document.getElementById('romazi').textContent = nowword;
+}
 
-let quoteIndex; // 現在の引用文のインデックス
-let currentWordIndex; // 現在の単語のインデックス
-let currentWord; // 現在の単語
-let typedWord; // 入力された文字列
+function randomnumber(nownum) {
+  var maxValue = word.length - 1;
+  var randomNumber = Math.floor(Math.random() * maxValue) + 1; // 1からmaxValueまでのランダムな数を生成
 
-function startGame() {
-  quoteIndex = Math.floor(Math.random() * quotes.length); // ランダムな引用文のインデックスを設定
-  currentWordIndex = 0; // 最初の単語のインデックスを設定
-  currentWord = quotes[quoteIndex].split(' ')[currentWordIndex]; // 現在の単語を設定
-  typedWord = ''; // 入力された文字列を初期化
+  while (randomNumber === nownum || randomNumber % 2 === 0) {
+      randomNumber = Math.floor(Math.random() * maxValue) + 1; // もう一度ランダムな数を生成
+  }
 
-  document.getElementById('quote').textContent = quotes[quoteIndex]; // 引用文を表示
-  document.getElementById('input').textContent = ''; // 入力文字列を初期化
-  document.getElementById('result').textContent = 'スペースキーを押してスタート'; // ゲームのスタートメッセージを表示
-
-  document.addEventListener('keydown', handleKeyDown); // キーボードのキーを監視するイベントリスナーを追加
+  return randomNumber;
 }
 
 function handleKeyDown(event) {
-  if (event.key === ' ') {
-    return; // スペースキーが押された場合は処理を中断
-  }
-
-  typedWord += event.key; // 入力された文字を追加
-  updateInputDisplay(); // 入力文字列の表示を更新
-
-  if (typedWord === currentWord) { // 入力文字列が現在の単語と一致する場合
-    document.getElementById('input').classList.remove('incorrect'); // 'incorrect'クラスを削除
-    document.getElementById('input').classList.add('correct'); // 'correct'クラスを追加
-    typedWord = ''; // 入力文字列を初期化
-    currentWordIndex++; // 次の単語のインデックスに進める
-
-    if (currentWordIndex >= quotes[quoteIndex].split(' ').length) { // 現在の引用文のすべての単語を入力した場合
-      quoteIndex++; // 次の引用文のインデックスに進める
-
-      if (quoteIndex >= quotes.length) { // すべての引用文を入力した場合
-        endGame(); // ゲーム終了
-        return;
+  var tangomozisu = nowword.length
+  if (event.key == nowword[nanmozime]) {
+      nanmozime = nanmozime + 1;
+      alltype = alltype + 1;
+      
+      document.getElementById('romazi').innerHTML  = '<span class="green">'+nowword.substring(0, nanmozime)+'</span>'+nowword.substring(nanmozime,tangomozisu)
+      if(tangomozisu == nanmozime) {
+          gamestart()
       }
-
-      currentWordIndex = 0; // 次の引用文の最初の単語のインデックスにリセット
-      currentWord = quotes[quoteIndex].split(' ')[currentWordIndex]; // 次の単語を設定
-      document.getElementById('quote').textContent = quotes[quoteIndex]; // 次の引用文を表示
-    } else {
-      currentWord = quotes[quoteIndex].split(' ')[currentWordIndex]; // 次の単語を設定
-    }
-
-    updateInputDisplay(); // 入力文字列の表示を更新
-  } else {
-    document.getElementById('input').classList.add('incorrect'); // 入力文字列が正しくない場合、'incorrect'クラスを追加
+  }else{
+      miss = miss + 1;
+      document.getElementById('romazi').innerHTML  = '<span class="green">'+nowword.substring(0, nanmozime)+'</span><span class="red">'+nowword.substring(nanmozime,nanmozime+1)+'</span>'+nowword.substring(nanmozime+1,tangomozisu)
   }
 }
 
-function updateInputDisplay() {
-  const inputElement = document.getElementById('input');
-  const correctTypedWord = typedWord.length <= currentWord.length ? typedWord : typedWord.slice(0, -1); // 正しく入力された文字列
-  const remainingWord = currentWord.slice(correctTypedWord.length); // 残りの文字列
 
-  inputElement.innerHTML = `<span class="correct">${correctTypedWord}</span><span class="remaining">${remainingWord}</span>`; // 入力文字列を更新して表示
-}
 
-function endGame() {
-  document.getElementById('input').classList.remove('correct', 'incorrect'); // 'correct'および'incorrect'クラスを削除
-  document.getElementById('input').textContent = ''; // 入力文字列を初期化
-  document.getElementById('result').textContent = 'ゲーム終了！'; // ゲーム終了メッセージを表示
-  document.removeEventListener('keydown', handleKeyDown); // キーボードのキー監視イベントリスナーを削除
-}
-
-startGame()
+document.addEventListener('keydown', handleKeyDown);
+gamestart();
